5d03b64e5ede8fcf8f6a7279ea742003aabbcee9
Sean McAfee <eefacm@gmail.com>
2016-07-16 09:24:16 -0700

    GPLize

--- boardgaming-online.js
+++ boardgaming-online.js
@@ -1,6 +1,24 @@
 "use strict";
 
-//
+// Copyright 2016 Sean McAfee
+
+// This file is part of conkeror-boardgaming.
+
+// conkeror-boardgaming is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+
+// conkeror-boardgaming is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with conkeror-boardgaming.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+
 //  A page mode for the web site boardgaming-online.com, which offers
 //  a web-based implementation of the excellent board game "Through
 //  the Ages: A Story of Civilization."
